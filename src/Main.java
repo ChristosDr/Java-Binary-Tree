79396c85e65ce56e6b1a25f1462470331f20b66a
chris <drikosx323@gmail.com>
2024-03-14 15:36:25 +0200

    Added createBinaryTree method Added preOrder method to print tree elements

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 
+    private TreeNode root;
+
     public class TreeNode{
         private int data;
         private TreeNode left;
@@ -10,8 +12,34 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public void createBinaryTree(){
+        TreeNode first = new TreeNode(1);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(4);
+        TreeNode five = new TreeNode(5);
 
-        System.out.println("Hello world!");
+        root = first;
+        first.left = second;
+        first.right = third;
+
+        second.left = fourth;
+        second.right = five;
+    }
+
+    public void preOrder(TreeNode root){
+        if (root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Main sll = new Main();
+        sll.createBinaryTree();
+        sll.preOrder(sll.root);
+
     }
 }
