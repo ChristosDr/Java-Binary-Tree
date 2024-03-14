f4bcac3faa8a2990508582edcefa4d962551eb8e
chris <drikosx323@gmail.com>
2024-03-14 15:17:01 +0200

    Implemented TreeNode

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public class TreeNode{
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
