be64cfafd849f0719bd81f7e0471ff961a607844
chris <drikosx323@gmail.com>
2024-03-28 15:15:10 +0200

    Added findMax method

--- src/Main.java
+++ src/Main.java
@@ -118,6 +118,22 @@ public class Main {
         }
     }
 
+    public int findMax(TreeNode root){
+        if (root == null){
+            return Integer.MIN_VALUE;
+        }
+        int result = root.data;
+        int left = findMax(root.left);
+        int right = findMax(root.right);
+        if (left > result){
+            result = left;
+        }
+        if (right > result){
+            result = right;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Main sll = new Main();
         sll.createBinaryTree();
@@ -126,7 +142,9 @@ public class Main {
         //sll.inOrder(sll.root);
         //sll.inOrderIterative();
         //sll.postOrder(sll.root);
-        sll.postOrderIterative();
+        //sll.postOrderIterative();
+        System.out.println(sll.findMax(sll.root));
+
 
     }
 }
